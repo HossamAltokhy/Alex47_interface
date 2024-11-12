b7bd04601a5e3d93568a1217212e218b42373e41
HossamAltokhy <hossam.abdulghany853@gmail.com>
2024-11-12 17:58:00 +0200

    First moment at ALEX47 interfacing

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: hossa
+ *
+ * Created on November 12, 2024, 5:50 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
